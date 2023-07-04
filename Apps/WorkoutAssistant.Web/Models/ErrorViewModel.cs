65323cb6a2bee247ba571591e3595abe61a18937
AhadVirus <ahadvirus@yahoo.com>
2023-07-04 18:35:29 +0330

    Initial Commit

---
+++ Apps/WorkoutAssistant.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WorkoutAssistant.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
