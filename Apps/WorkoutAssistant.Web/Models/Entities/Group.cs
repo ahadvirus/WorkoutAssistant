f56e058a28f50597bb0fd0ed1b4ef18e3ec2a498
AhadVirus <ahadvirus@yahoo.com>
2023-07-04 19:58:45 +0330

    Define group entity for divided all exercises in different group

---
+++ Apps/WorkoutAssistant.Web/Models/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WorkoutAssistant.Web.Models.Entities;
+
+public class Group
+{
+    /// <summary>
+    /// The primary key of table
+    /// </summary>
+    public virtual Guid Id { get; set; }
+    
+    /// <summary>
+    /// Name of the group exercise
+    /// </summary>
+    public virtual string Name { get; set; } = string.Empty;
+}
