d2049d53d6956fbcd90ae04a0f479982947172b6
AhadVirus <ahadvirus@yahoo.com>
2023-07-04 20:01:10 +0330

    Create base classes for migrations and all indexes in tables

---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Database/TableMigration.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace WorkoutAssistant.Web.Infrastructures.Database;
+
+public abstract class TableMigration : Migration
+{
+    /// <summary>
+    /// Represent the name of table in database
+    /// </summary>
+    protected abstract string TableName { get; }
+
+    /// <summary>
+    /// Represent all indexes can be use in migrations for all tables
+    /// </summary>
+    protected TablePrimaryIndexes Indexes
+    {
+        get
+        {
+            return new TablePrimaryIndexes();
+        }
+    }
+}
---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Database/TablePrimaryIndexes.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutAssistant.Web.Infrastructures.Database.EntitiesPrimaryIndex;
+
+namespace WorkoutAssistant.Web.Infrastructures.Database;
+
+public class TablePrimaryIndexes
+{
+    public GroupIndexes Group
+    {
+        get
+        {
+            return new GroupIndexes();
+        }
+    }
+}
