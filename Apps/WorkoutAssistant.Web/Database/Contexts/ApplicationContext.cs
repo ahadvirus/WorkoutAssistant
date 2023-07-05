915986be3a3453aa2f3b372c896a993fe64542f3
AhadVirus <ahadvirus@yahoo.com>
2023-07-05 10:25:25 +0330

    Implement context system to application

---
+++ Apps/WorkoutAssistant.Web/Database/Contexts/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkoutAssistant.Web.Database.Connections;
+using WorkoutAssistant.Web.Models.Entities;
+
+namespace WorkoutAssistant.Web.Database.Contexts;
+
+public class ApplicationContext : DbContext
+{
+    /// <summary>
+    /// Connection string to connect database
+    /// </summary>
+    protected GymSqlConnection Connection { get; }
+    
+    public ApplicationContext(DbContextOptions<ApplicationContext> options, GymSqlConnection connection) : base(options: options)
+    {
+        Connection = connection;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString: Connection.ConnectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    /// <summary>
+    /// Access to group table in database
+    /// </summary>
+    public DbSet<Group> Groups { get; set; }
+}
