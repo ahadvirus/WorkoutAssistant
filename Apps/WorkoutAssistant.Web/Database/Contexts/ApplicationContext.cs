4c40ae3fe1a81efd960afa265d85c47ac38d1353
AhadVirus <ahadvirus@yahoo.com>
2023-07-06 10:17:02 +0330

    Create DbContext for system

--- Apps/WorkoutAssistant.Web/Database/Contexts/ApplicationContext.cs
+++ Apps/WorkoutAssistant.Web/Database/Contexts/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WorkoutAssistant.Web.Database.Configurations;
 using WorkoutAssistant.Web.Database.Connections;
 using WorkoutAssistant.Web.Models.Entities;
 
@@ -9,11 +10,18 @@ public class ApplicationContext : DbContext
     /// <summary>
     /// Connection string to connect database
     /// </summary>
-    protected GymSqlConnection Connection { get; }
-    
-    public ApplicationContext(DbContextOptions<ApplicationContext> options, GymSqlConnection connection) : base(options: options)
+    protected WorkoutSqlConnection Connection { get; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options, WorkoutSqlConnection connection) : base(
+        options: options)
     {
         Connection = connection;
+
+        #region InitialTables
+
+        Groups = Set<Group>();
+
+        #endregion
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -22,8 +30,23 @@ public class ApplicationContext : DbContext
         base.OnConfiguring(optionsBuilder);
     }
 
+    #region DefineTables
+
     /// <summary>
     /// Access to group table in database
     /// </summary>
     public DbSet<Group> Groups { get; set; }
+
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region Configurations
+
+        modelBuilder.ApplyConfiguration(new GroupConfiguration());
+
+        #endregion
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
