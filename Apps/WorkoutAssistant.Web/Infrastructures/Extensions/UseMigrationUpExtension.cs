5871508e814a5c5b9289528fe5399cffca9f494c
AhadVirus <ahadvirus@yahoo.com>
2023-07-06 11:01:54 +0330

    Create extension to apply migrations to database

---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Extensions/UseMigrationUpExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WorkoutAssistant.Web.Infrastructures.Contracts;
+
+namespace WorkoutAssistant.Web.Infrastructures.Extensions;
+
+public static class UseMigrationUpExtension
+{
+    public static async void UseMigrationUp(this IApplicationBuilder entry)
+    {
+        using (IServiceScope scope = entry.ApplicationServices.CreateScope())
+        {
+            IMigratorService?
+                service = scope.ServiceProvider.GetService<IMigratorService>();
+
+            if (service != null)
+            {
+                await service.MigrateUpAsync();
+            }
+        }
+        
+        //return entry;
+    }
+}
