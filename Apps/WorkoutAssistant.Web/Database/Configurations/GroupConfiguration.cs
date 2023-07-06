2dd2b71911bcc81d6dcd27a19dcc04d103602ecc
AhadVirus <ahadvirus@yahoo.com>
2023-07-06 10:57:08 +0330

    Change name space of reorganization

---
+++ Apps/WorkoutAssistant.Web/Database/Configurations/GroupConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutAssistant.Web.Models.Entities;
+
+namespace WorkoutAssistant.Web.Database.Configurations;
+
+public class GroupConfiguration : IEntityTypeConfiguration<Group>
+{
+    public void Configure(EntityTypeBuilder<Group> builder)
+    {
+        builder.HasKey(keyExpression: group => group.Id);
+
+        builder.Property(propertyExpression: group => group.Name)
+            .IsRequired();
+
+        builder.HasIndex(indexExpression: group => group.Name);
+    }
+}
