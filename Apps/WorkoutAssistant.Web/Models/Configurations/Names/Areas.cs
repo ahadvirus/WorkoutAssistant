b789c33704adc60175ff547a07ea7fd1ef2966d0
AhadVirus <ahadvirus@yahoo.com>
2023-07-07 09:05:19 +0330

    Trying create hierarchy system for application and create configuration files for it

---
+++ Apps/WorkoutAssistant.Web/Models/Configurations/Names/Areas.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkoutAssistant.Web.Models.Configurations.Names;
+
+/// <summary>
+/// Represent all area names exist in application
+/// </summary>
+public class Areas
+{
+    /// <summary>
+    /// Return name of admin area
+    /// </summary>
+    public string Admin
+    {
+        get
+        {
+            return nameof(Admin);
+        }
+    }
+
+    /// <summary>
+    /// Return name of auth area
+    /// </summary>
+    public string Auth { get; set; }
+}
---
+++ Apps/WorkoutAssistant.Web/Models/Configurations/Names/View.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutAssistant.Web.Models.Configurations.Names;
+
+/// <summary>
+/// Define all static names shared between views
+/// </summary>
+public class View
+{
+    /// <summary>
+    /// Represent all title in views
+    /// </summary>
+    public string Title
+    {
+        get
+        {
+            return nameof(Title);
+        }
+    }
+}
