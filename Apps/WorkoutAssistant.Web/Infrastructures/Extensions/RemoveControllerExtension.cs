fc825470a602862616d2ee034b0bdaa0f13f88ff
AhadVirus <ahadvirus@yahoo.com>
2023-07-10 14:17:51 +0330

    Create extension to replace controller phrase to empty in string

---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Extensions/RemoveControllerExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutAssistant.Web.Infrastructures.Extensions;
+
+public static class RemoveControllerExtension
+{
+    /// <summary>
+    /// Remove Controller in string
+    /// </summary>
+    /// <param name="name"><see cref="string"/>The string you want remove controller from it</param>
+    /// <returns><see cref="string"/>Return a string without controller in it</returns>
+    public static string RemoveController(this string name)
+    {
+        return name.Replace(oldValue: nameof(Controller), newValue: string.Empty);
+    }
+}
