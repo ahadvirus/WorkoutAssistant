bd9c37ec325de828d77a4f9bfb527e0286957de6
AhadVirus <ahadvirus@yahoo.com>
2023-07-12 14:57:30 +0330

    Create configuration for RoleUser

---
+++ Apps/WorkoutAssistant.Web/Database/Configurations/RoleUserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutAssistant.Web.Infrastructures.Database.Configurations.RoleUsers;
+using WorkoutAssistant.Web.Infrastructures.Database.Contracts.Configurations;
+using WorkoutAssistant.Web.Models.Entities;
+
+namespace WorkoutAssistant.Web.Database.Configurations;
+
+public class RoleUserConfiguration : EntityConfiguration<RoleUser, Guid, RoleUserTable, RoleUserTableColumns, RoleUserTableIndex, RoleUserTableRows>
+{
+    public override void Configure(EntityTypeBuilder<RoleUser> builder)
+    {
+        builder.HasKey(keyExpression: roleUser => roleUser.Id)
+            .HasName(name: Configuration.ColumnsName.Primary);
+
+        builder.Property(propertyExpression: roleUser => roleUser.RoleId)
+            .HasColumnName(name: Configuration.ColumnsName.RoleId)
+            .IsRequired();
+        
+        builder.Property(propertyExpression: roleUser => roleUser.UserId)
+            .HasColumnName(name: Configuration.ColumnsName.UserId)
+            .IsRequired();
+        
+        builder.HasIndex(indexExpression: roleUser => new { roleUser.RoleId, roleUser.UserId }, name: Configuration.IndexesName.RoleIdUserIdUnique)
+            .IsUnique();
+
+        builder.ToTable(name: Configuration.TableName);
+    }
+}
