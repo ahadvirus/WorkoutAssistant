c310e6b311db9729a99599132945b8865bf8f0fa
AhadVirus <ahadvirus@yahoo.com>
2023-07-12 09:37:13 +0330

    Create base system for entity framework configuration to be able use table configuration

---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Database/Contracts/Configurations/EntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutAssistant.Web.Infrastructures.Contracts;
+using WorkoutAssistant.Web.Infrastructures.Database.Contracts.Migrations;
+
+namespace WorkoutAssistant.Web.Infrastructures.Database.Contracts.Configurations;
+
+public abstract class EntityConfiguration<TEntity, TPrimary, TTable, TColumns, TIndexes, TRows> :
+    IEntityTypeConfiguration<TEntity>,
+    ITableConfiguration<TTable, TColumns, TIndexes, TRows>
+    where TTable : class, ITable<TColumns, TIndexes, TRows>, new()
+    where TColumns : ITableColumnsDefinition
+    where TIndexes : TableIndexes<TColumns>
+    where TEntity : class, IEntity<TPrimary>
+    where TPrimary : struct
+{
+    public abstract void Configure(EntityTypeBuilder<TEntity> builder);
+
+    public TTable Configuration
+    {
+        get { return new TTable(); }
+    }
+}
