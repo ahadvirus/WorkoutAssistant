7734f4721a5e0efb8531bce1fe9f0ceefe4a127a
AhadVirus <ahadvirus@yahoo.com>
2023-07-12 10:10:20 +0330

    Create RoleUser entity to connect user to role

---
+++ Apps/WorkoutAssistant.Web/Models/Entities/RoleUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WorkoutAssistant.Web.Infrastructures.Contracts;
+
+namespace WorkoutAssistant.Web.Models.Entities;
+
+public class RoleUser : IEntity<Guid>
+{
+    /// <summary>
+    /// The primary key in table
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The foreign key for role table
+    /// </summary>
+    public Guid RoleId { get; set; }
+
+    /// <summary>
+    /// The foreign key for user table
+    /// </summary>
+    public Guid UserId { get; set; }
+}
