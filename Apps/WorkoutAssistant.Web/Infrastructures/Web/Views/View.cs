4076c6ae1d12df4ba6d3b88660f3aa8aa20aaf2c
AhadVirus <ahadvirus@yahoo.com>
2023-07-27 13:57:21 +0330

    Create base class for view and inherit to views

---
+++ Apps/WorkoutAssistant.Web/Infrastructures/Web/Views/View.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using WorkoutAssistant.Web.Models.Configurations;
+
+namespace WorkoutAssistant.Web.Infrastructures.Web.Views;
+
+public abstract class View<T> : RazorPage<T>
+{
+    protected Named Named
+    {
+        get
+        {
+            return new Named();
+        }
+    }
+    
+    [RazorInject]
+    public IViewLocalizer ViewLocalizer { get; set; }
+}
