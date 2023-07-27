d07dfbb3b9f13e3b059a745a6146766cb1713b89
AhadVirus <ahadvirus@yahoo.com>
2023-07-27 13:59:18 +0330

    Define all sections will be use in the view

---
+++ Apps/WorkoutAssistant.Web/Models/Configurations/Names/Sections.cs
@@ -0,0 +1,40 @@
+﻿namespace WorkoutAssistant.Web.Models.Configurations.Names;
+
+/// <summary>
+/// All sections exists in view
+/// </summary>
+public record Sections
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Title
+    {
+        get
+        {
+            return nameof(Title);
+        }
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Styles
+    {
+        get
+        {
+            return nameof(Styles);
+        }
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Scripts
+    {
+        get
+        {
+            return nameof(Scripts);
+        }
+    }
+}
